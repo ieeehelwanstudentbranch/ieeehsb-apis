a4297700a7c132cb14bb1e7c75bd8ba3c8088423
BolBol54 <zeka.bolbol@gmail.com>
2020-07-27 22:42:30 +0200

    databaseError

--- database/migrations/2020_06_16_134024_create_chapters_table.php
+++ database/migrations/2020_06_16_134024_create_chapters_table.php
@@ -18,8 +18,8 @@ class CreateChaptersTable extends Migration
             $table->string('name',200);
             $table->string('description',2000)->nullable();
             $table->string('logo',2000);
-            $table->unsignedBigInteger('chairperson_id')->after('logo')->nullable();
-            $table->foreign('chairperson_id')->references('id')->on('volunteers');            
+            $table->unsignedBigInteger('chairperson_id')->nullable();
+            $table->foreign('chairperson_id')->references('id')->on('volunteers');
             $table->timestamps();
         });
     }
